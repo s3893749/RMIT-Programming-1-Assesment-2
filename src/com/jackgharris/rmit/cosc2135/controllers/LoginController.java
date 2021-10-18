8cee7d5962ba95cffa0dd611742845d246d6703d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-18 17:18:54 +1100

    - Created Placeholder LoginController Class

---
+++ src/com/jackgharris/rmit/cosc2135/controllers/LoginController.java
@@ -0,0 +1,10 @@
+package com.jackgharris.rmit.cosc2135.controllers;
+
+import com.jackgharris.rmit.cosc2135.core.Controller;
+
+public class LoginController extends Controller{
+    @Override
+    public void boot() {
+
+    }
+}
