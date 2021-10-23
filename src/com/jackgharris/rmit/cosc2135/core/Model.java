4515514c07018241be4e123625138e6196f39880
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-23 15:49:05 +1100

    - Finished Justification comments Model parent

---
+++ src/com/jackgharris/rmit/cosc2135/core/Model.java
@@ -0,0 +1,9 @@
+//**** SET PACKAGE ****\\
+package com.jackgharris.rmit.cosc2135.core;
+
+//**** CLASS START ****\\
+//Justification:
+//this is a blank class that all models will extend, this allows me to store the models in the same array with in the main kernel class
+public class Model {
+    //bank class, inheritance used for storing multiple classes in the same array
+}
